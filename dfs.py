178c1e68b30ca0d699c621acfa5969e928b03ade
bng <patrick.acheson@hotmail.com>
2025-02-05 23:49:03 +0000

    working

--- dfs.py
+++ dfs.py
@@ -1,54 +1,65 @@
-import json
+class jseek_demo():
+    def __init__(self):
+        import json
+        with open('test.json', 'r') as f:
+            self.json_data = json.load(f)
 
-with open('test.json', 'r') as f:
-    json_data = json.load(f)
-
-def find_path_to_value(json_data, target_value):
-    queue = [("", json_data)]
-    
-    while queue:
-        current_path, current_value = queue.pop(0)
+    def find_path_to_value(self, target_value):
+        queue = [("", self.json_data)]
         
-        if isinstance(current_value, dict):
-            for k, v in current_value.items():
-                new_path = f"{current_path}.{k}" if current_path else k
-                queue.append((new_path, v))
-        elif isinstance(current_value, list):
-            for index, item in enumerate(current_value):
-                new_path = f"{current_path}[{index}]" if current_path else f"[{index}]"
-                queue.append((new_path, item))
-        else:
-            if current_value == target_value:
-                return current_path
-    return None
+        while queue:
+            current_path, current_value = queue.pop(0)
+            
+            if isinstance(current_value, dict):
+                for k, v in current_value.items():
+                    new_path = f"{current_path}.{k}" if current_path else k
+                    queue.append((new_path, v))
+            elif isinstance(current_value, list):
+                for index, item in enumerate(current_value):
+                    new_path = f"{current_path}[{index}]" if current_path else f"[{index}]"
+                    queue.append((new_path, item))
+            else:
+                if current_value == target_value:
+                    return current_path
+        return None
 
-goal = "c"
-result = find_path_to_value(json_data, goal)
-
-usable_path = ""
-start = True
-skip = False
-for index, letter in enumerate("[" + result):
-    if skip:
+    def clean_path(self, path):
+        usable_path = ""
+        start = True
         skip = False
-        continue
-    print("debug:", index, letter)
-    if letter == ".":
-        if result[index - 2] == "]":
-            usable_path += "["
-            continue
-        usable_path += "]["
-    elif (index) < len(result) -1:
-        if result[index + 1] == "[":
-            usable_path += f"{result[index - 1]}{result[index]}]"
-            skip = True
-        else:
-            usable_path += str(letter)
-    else:
-        usable_path += str(letter)
+        for index, letter in enumerate("[" + path):
+            if skip:
+                skip = False
+                continue
+            if letter == ".":
+                if path[index - 2] == "]":
+                    usable_path += "["
+                    continue
+                usable_path += "]["
+            elif (index) < len(path) - 1:
+                if path[index + 1] == "[":
+                    usable_path += f"{path[index - 1]}{path[index]}]"
+                    skip = True
+                else:
+                    usable_path += str(letter)
+            else:
+                usable_path += str(letter)
+        
+        import re
+        def add_quotes(match):
+            key = match.group(1)
+            if key.isdigit():
+                return f"[{key}]"
+            else:
+                return f"['{key}']"
+        
+        usable_path = re.sub(r"\[([^\[\]]+)\]", add_quotes, usable_path)
+        return usable_path
+    
+
+jseek = jseek_demo()
+path = jseek.find_path_to_value("c")
+usable_path = jseek.clean_path(path)
 
 print("\n", usable_path)
 
-# print(json_data)
-
-print(f"Path to '{goal}': {result}")
